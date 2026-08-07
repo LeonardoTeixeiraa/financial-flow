56a288182a71173ecb60e68c40f83f9b2bbe7c11
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-08-06 22:47:06 -0300

    feat: implementar classe de repository para User

---
+++ src/main/java/com/leonardoteixeira/pdfextractor/identity/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.leonardoteixeira.pdfextractor.identity.repository;
+
+import com.leonardoteixeira.pdfextractor.identity.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
